2fe469850d91d401b538385fe5b9b07ae9ab98ac
rrana157 <66181815+rrana157@users.noreply.github.com>
2025-05-27 11:44:36 +0530

    Added some setup files.

---
+++ DataForge/main.py
@@ -0,0 +1,13 @@
+
+
+def hello():
+    print("Hello from Jack")
+
+
+
+
+# python setup.py sdist bdist_wheel
+"""
+This command will generate two distributions for us. A source distribution, which is usually just the python scripts,
+and a wheel distribution. so this usually also contains binaries, which are platform specific.
+"""
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DataForge',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here.
+        # e.g. 'numpy>=1.11.1
+    ]
+)
+
